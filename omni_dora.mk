9c57a0c6d24f0622a0d3f5379847b29db2844785
Humberto Borba <humberos@gmail.com>
2017-12-23 16:46:02 -0200

    sony: dora: Omnify
    Change-Id: I93b6ba7eb09919d92a114777c556a56ed34be78d
    Signed-off-by: Humberto Borba <humberos@gmail.com>

--- omni_dora.mk
+++ omni_dora.mk
@@ -1,4 +1,3 @@
-#
 # Copyright 2014 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,6 +11,21 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
-add_lunch_combo omni_dora-userdebug
+# Bootanimation
+TARGET_BOOTANIMATION_SIZE := 1080p
+
+# Inherit device parts
+$(call inherit-product, device/sony/dora/aosp_f8131.mk)
+
+# Override Product Name
+PRODUCT_NAME := omni_dora
+PRODUCT_MODEL := Xperia X Performance
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := none
+
+# Inherit rom parts
+$(call inherit-product, device/sony/common/omni.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
